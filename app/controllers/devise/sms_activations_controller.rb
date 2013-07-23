827247d353bb69fe238bb742c6e8f1bd32b0cf4f
sstarke <sirsupertramp@gmail.com>
2013-07-23 18:20:03 +0200

    Added find_messages method to controller.

--- app/controllers/devise/sms_activations_controller.rb
+++ app/controllers/devise/sms_activations_controller.rb
@@ -41,4 +41,13 @@ class Devise::SmsActivationsController < DeviseController
       new_session_path(resource_name) if is_navigational_format?
     end
 
+  # Get message for given
+  def find_message(kind, options = {})
+    options[:scope] = "devise.#{controller_name}"
+    options[:default] = Array(options[:default]).unshift(kind.to_sym)
+    options[:resource_name] = resource_name
+    options = devise_i18n_options(options) if respond_to?(:devise_i18n_options, true)
+    I18n.t("#{options[:resource_name]}.#{kind}", options)
+  end
+
 end
