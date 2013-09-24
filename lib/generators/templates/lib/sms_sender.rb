41a407abaf8b7b4ccb704ed854850733944f5b48
Yash Shah <mail@yashshah.com>
2013-09-24 19:40:55 +0530

    Fix template to support auto load of lib in Rails >=3.0

--- lib/generators/templates/lib/sms_sender.rb
+++ lib/generators/templates/lib/sms_sender.rb
@@ -1,7 +1,10 @@
-class Devise::SmsSender
-  #Actually sends the sms token. feel free to modify and adapt to your provider and/or gem
-  def self.send_sms(phone,message)
-    sms = Moonshado::Sms.new(phone, message)
-    return sms.deliver_sms[:stat]
-  end
+module Devise
+	class SmsSender
+	  #Actually sends the sms token. feel free to modify and adapt to your provider and/or gem
+	  def self.send_sms(phone,message)
+	    sms = Moonshado::Sms.new(phone, message)
+			puts message
+	    return sms.deliver_sms[:stat]
+	  end
+	end
 end
